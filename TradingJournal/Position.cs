7cc36e4d476f63b8decffb4df9e748efdf44b45d
Kevin Hepp <kevin_hepp@trimble.com>
2016-11-29 08:43:45 -0700

    test push

---
+++ TradingJournal/Position.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace TradingJournal
+{
+    public class Position
+    {
+        public string Ticker { get; set; }                        
+        public bool Closed { get; set; }
+
+
+
+
+    }
+
+    public class Trade
+    {
+        public InstrumentType Instrument { get; set; }
+
+    }
+
+    public enum InstrumentType
+    {
+        Common,
+        Call,
+        Put,
+        Future
+    }
+}
